8fb9f243e66e28c45583cafddf3e8c064bb8319c
pudiss <pudis.2550@gmail.com>
2020-12-20 20:27:57 +0700

    linked list

---
+++ linked list tutorial.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import pandas as pd 
+from matplotlib import pyplot as plt 
+
+# The data frame
+df = pd.read_csv(r"C:\Users\pudis\data science file\All csv file\appstore application\AppleStore.csv")
+
+print(df.columns)
