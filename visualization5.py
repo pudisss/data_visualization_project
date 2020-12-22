cd1257c1b92403bae784e59d3ee808494d8b5b37
Pudis <pudis.2550@gmail.com>
2020-12-22 09:56:17 +0700

    New function

--- visualization5.py
+++ visualization5.py
@@ -4,3 +4,30 @@ from matplotlib import pyplot as plt
 import sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+
+# The data frame
+df = pd.read_csv(r"C:\Users\pudis\data science file\All csv file\best seller book\bestsellers with categories.csv")
+# The data frame that doesn't have the nan value
+ndf = df.dropna(axis=0)
+
+def bar_user_rating():
+	# The variable that use for fill the data from the data frame
+	name = []
+	rating = []
+	price = []
+	review = []
+	author = []
+	col = ["User Rating", "Price", "Reviews", "Author"]
+	var = [rating, price, review, author]
+	for j in range(len(var)):
+		for d in ndf[col[j]][:255]:
+			var[j].append(d)
+	for n in ndf["Name"]:
+		name.append(n)
+
+	# To make the plot chart 
+	plt.plot(range(len(rating)), rating, linestyle=":", linewidth=2, color="y")
+	plt.show()
+
+
+bar_user_rating()
